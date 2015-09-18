ef2fccc584e55333f76b41cea5902b94a14aed4d
zhuchen <zhuchen@ubuntu.(none)>
2015-09-17 22:38:21 -0400

    Delete redundant else block

--- src/edu/upenn/cis573/hwk1/Main.java
+++ src/edu/upenn/cis573/hwk1/Main.java
@@ -12,38 +12,32 @@ public class Main {
 			System.out.println("The number of runtime arguments is not correct");
 			return;
 		}
-		else{
-			File dir = new File(args[0]);
-			if (!dir.isDirectory()) {
-				System.out.println("The specified directory does not exist");
-				return;
-			}
-			else{
-				File[] directoryListing = dir.listFiles();
-				if (directoryListing.length == 0) {
-					System.out.println("The specified directory is empty.");
-					return;
-				}
-				else{
-					for (File file : directoryListing) {
-						Util u = new Util();
-						String originaltext = "";
-						String encrypted = "";
-						originaltext = u.convertFileToString(file);
-						encrypted = u.encrypt(originaltext);
-						int[] frequency = new int[26];
-						for (File file1 : directoryListing) {
-							if (!file1.getName().equals(file.getName())) {
-								String text = u.convertFileToString(file1);
-								u.getFrequencyTable(text, frequency);
-							}
-						}
-						String decrypted = u.decrypt(encrypted, frequency);
-						u.compareAndCount(originaltext, decrypted, file.getName());
-						}
-					Util.printSummary(totalCount, totalCorrect);
-				}
-			} 
+		File dir = new File(args[0]);
+		if (!dir.isDirectory()) {
+			System.out.println("The specified directory does not exist");
+			return;
 		}
+		File[] directoryListing = dir.listFiles();
+		if (directoryListing.length == 0) {
+			System.out.println("The specified directory is empty.");
+			return;
+		}
+		for (File file : directoryListing) {
+			Util u = new Util();
+			String originaltext = "";
+			String encrypted = "";
+			originaltext = u.convertFileToString(file);
+			encrypted = u.encrypt(originaltext);
+			int[] frequency = new int[26];
+			for (File file1 : directoryListing) {
+				if (!file1.getName().equals(file.getName())) {
+					String text = u.convertFileToString(file1);
+					u.getFrequencyTable(text, frequency);
+				}
+			}
+			String decrypted = u.decrypt(encrypted, frequency);
+			u.compareAndCount(originaltext, decrypted, file.getName());
+			}
+		Util.printSummary(totalCount, totalCorrect);
 	}
 }
