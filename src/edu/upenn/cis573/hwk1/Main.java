786e84208f384b6b7c3f6e3dde241aa3c0854666
zhuchen <zhuchen@ubuntu.(none)>
2015-09-17 22:41:32 -0400

    modified indentation

--- src/edu/upenn/cis573/hwk1/Main.java
+++ src/edu/upenn/cis573/hwk1/Main.java
@@ -37,7 +37,7 @@ public class Main {
 			}
 			String decrypted = u.decrypt(encrypted, frequency);
 			u.compareAndCount(originaltext, decrypted, file.getName());
-			}
+		}
 		Util.printSummary(totalCount, totalCorrect);
 	}
 }
